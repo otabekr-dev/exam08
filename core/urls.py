b6748484f610b4d4d011791442c3140a7ef38c5b
otabek7997 <reyimbayevotabek7@gmail.com>
2026-01-28 09:31:03 +0500

    fix

--- core/urls.py
+++ core/urls.py
@@ -7,14 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('events/', include('events.urls')),
-
-
-
-
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional: Swagger UI and ReDoc views
-    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 
